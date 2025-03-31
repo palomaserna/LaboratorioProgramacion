f06ccac73cbaf43ae1955029e38efe996533ee73
PalomaSerna <palomaserna55@gmail.com>
2025-03-31 11:50:21 -0300

    Agregar archivos de proyecto.

---
+++ LaboratorioProgramacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2022!");
+        }
+    }
+}
